f652b8828f0c237871f24866b8b830d37cac7276
h0mieSapiens <51687450+h0mieSapiens@users.noreply.github.com>
2019-08-26 18:12:52 -0700

    Add files via upload

---
+++ lab1/app.js
@@ -0,0 +1,41 @@
+
+
+function smaller(num1, num2) {
+    if (num1 < num2)
+        console.log(num1);
+    else
+        console.log(num2);
+}
+
+function isEven(number) {
+    var res = number % 2;
+    if (res == 0) return true;
+    else
+        return false;
+}
+
+function printE(cuantos) {
+var suf=0;
+
+    for (i = 0;  1<=cuantos; i++) {
+        
+        var res = number % 2;
+        if (res == 0) console.log(res){  }       suf=suf+1;
+    }
+}
+
+function toupper(text) {
+
+    text.toUpperCase('miguel');
+}
+
+function init() {
+    console.log("miguel");
+    var res = smaller(2, 9);
+    console.log(res);
+
+    var res2 = smaller(89827349837492834792, 8379287493287498);
+    console.log(res2);
+
+}
+printE(5000);
---
+++ lab1/even.js
@@ -0,0 +1,10 @@
+function printE(cuantos) {
+    var c = 0;
+    for (i = 0; i <= cuantos; i++) {
+        c = c + 2
+        console.log(c)
+       
+        }
+    
+}
+printE(5);
